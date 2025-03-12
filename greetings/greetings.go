a1736cbf5d55474b0306894c9090bd0aaa7f57ea
Samuel Scalise <sjs99d@gmail.com>
2025-03-12 14:24:37 -0500

    slice format greetings

--- greetings/greetings.go
+++ greetings/greetings.go
@@ -3,6 +3,7 @@ package greetings
 import (
 	"fmt"
 	"errors"
+	"math/rand"
 )
 
 // Hello returns a greeting for the named person.
@@ -14,6 +15,23 @@ func Hello (name string) (string, error) {
 	}	
 
 	//return a greeting that embeds the name in message
-	message := fmt.Sprintf("Hi, %v. Welcome!", name)
+	message := fmt.Sprintf(randomFormat(), name)
 	return message, nil
+}
+
+// randomFormat Returns one of a set of greeting messages. The returned
+// message is selected at random
+
+func randomFormat() string {
+	// A slice of message formats
+	formats:= []string{
+		"Hi, %v. Welcome!",
+		"Great to see you, %v!",
+		"Hail, %v! Well met!",
+	}
+
+	// Return the randomly selected message format by specifying a random index
+	// a random index for the slice formats
+	
+	return formats [rand.Intn(len(formats))]
 }
